ef515fad90888e1f9cabb8e5e05e2449899ca115
manfredhq <hugo.quitton@gmail.com>
2020-01-26 21:26:28 +0100

    added some weapon scripts

---
+++ Assets/scripts/Interface/IWeapon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IWeapon
+{
+    string name { get; set; }
+    int damage { get; set; }
+    
+    float bulletForce { get; set; } 
+    float RPM { get; set; }
+
+    public WeaponType weaponType { get; set; }
+}
+public enum WeaponType
+{
+    pistol,
+    shotgun
+}
---
+++ Assets/scripts/Weapons/Pistol.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : MonoBehaviour, IWeapon
+{
+    public int damage { get => damage; set => damage = value; }
+    public float bulletForce { get => bulletForce; set => bulletForce = value; }
+    public float RPM { get => RPM; set => RPM = value; }
+    public WeaponType weaponType { get => weaponType; set => weaponType = value; }
+
+
+    // Start is called before the first frame update
+    void Start(float rpm = 0.5f, int dmg = 1, float bulletSpeed = 20f)
+    {
+        RPM = rpm;
+        damage = dmg;
+        bulletForce = bulletSpeed;
+        weaponType = WeaponType.pistol;
+    }
+}
